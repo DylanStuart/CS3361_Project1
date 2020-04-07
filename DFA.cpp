31dcc474dbd864847eb30ee906c559e8f409aba2
DylanStuart <53539615+DylanStuart@users.noreply.github.com>
2020-04-07 05:39:07 -0500

    Add files via upload
    ignore the email I sent you earlier. made some changes.
    read the comments inside

--- DFA.cpp
+++ DFA.cpp
@@ -4,12 +4,19 @@
 #include <string>
 #include <sstream>
 #include <ctype.h>
+// was trying to add the extra options to isdigit and was getting errors.
+//its working for the exact data set that was provided in the project, however i'm pretty sure it will break if you change the DFA.txt file
+//will try and file tune it.
+// wasn't sure how to add spaces/newlines
+//not sure if we should add what states is on?
+
+
 
 using namespace std;
 int main() {
 	char DFA[20];
-	int count = 1;
-	string DFA_Result[20];
+	
+	string DFA_Result[30];
 	
 	int i = 0;
 	int k = 0;
@@ -80,18 +87,52 @@ int main() {
 			DFA_Result[k] = "Times";
 			k++;
 		}
+		if (isdigit(DFA[j])) {
+		
+			DFA_Result[k] = "Digit";
+			k++;
+		}
 
-		// was having issues with letter and diget i kept getting errors so erased that part for now. 
-		// I also think something is wrong with the 1st if statment for "div"
+		if (isalpha(DFA[j])) {
+			while (isalpha(DFA[j + 1])) {
+				j++;
+			}
+			if (DFA[j - 3] == 'r' &&  DFA[j - 2] == 'e' && DFA[j - 1] == 'a' && DFA[j] == 'd') {
+				DFA_Result[k] = "read";
+				k++;
+			}
+		 else if (DFA[j - 4] == 'w' &&  DFA[j - 3] == 'r' && DFA[j - 2] == 'i' && DFA[j - 1] == 't' && DFA[j] == 'e') {
+				DFA_Result[k] = "write";
+				k++;
+			}
+
+			else {
+				DFA_Result[k] = "Letter";
+				k++;
+
+			}
+
+
+			
+		}
+
+
 
 		
 		
 
 	}
-
+	myfile2 << "(";
 	for (int h = 0; h < k; h++) {
-		myfile2 << DFA_Result[h] << endl;
+		
+		myfile2 << DFA_Result[h];
+		if (h + 1 < k) {
+			myfile2 << ", ";
+		}
 	}
+	myfile2 << " )";
+
+	
 
 
 	
